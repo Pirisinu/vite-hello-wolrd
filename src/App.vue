aaa57f153c82b550134a100505004020120c3ce0
Pirisinu <palazzialessandro2@gmail.com>
2023-10-09 15:14:51 +0200

    [INIT]REPO + [ADD]variabile

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+  <script>
+  export default {
+    name: 'app',
+    data(){
+      return{
+        paragrafo: 'ciao??'
+      }
+    }
+  }
+  </script>
+
+<template>
+  <h1>{{paragrafo}}</h1>
+</template>
+
+<style>
+
+</style>
